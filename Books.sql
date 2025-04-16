38b137d1c925b4cc7ba4a9fd8938d7afaca7cbc3
Stefanie Sluschny <s.sluschny@web.de>
2025-04-16 11:14:37 +0200

    add Datensatz zu Books

--- Books.sql
+++ Books.sql
@@ -24,10 +24,16 @@ VALUES
 ("Safe", 'S.K. Barnett', 'Thriller', 3.9, 4.0),
 ("We Used to Live Here", 'Marcus Kliewer', 'Horror', 3.71, 5.0),
 ("What the Woods Took", 'Courtney Gould', 'Horror', 3.91, 3.0);
-*/
+
 
 INSERT INTO Books (Titel, Autor, Genre, grRating, MyRating)
 VALUES
 ("Hunted", "Darcy Coates", "Horror", 4.03, 4.0);
+
+INSERT INTO Books (Titel, Autor, Genre, grRating, MyRating)
+VALUES
+("Virtua", "Karl Olsberg", "Science-Fiction", 3.79, 4.0),
+("The Woman Who Fooled the World: Belle Gibson's Cancer Con, and the Darkness at the Heart of the Wellness Industry", "Beau Donelly, Nick Toscano", "Non-Fiction", 4.03, 4.0);
+*/
 
 SELECT * FROM Books;
